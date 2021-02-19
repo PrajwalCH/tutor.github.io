3894cdf8a86c4678ecca8ca4876bc5fa828ec286
Prajwal Chapagain <prajjwal2058@gmail.com>
2021-02-19 14:00:21 +0545

    Update buildIconTag.js

--- scripts/components/app_nav/tagBuilders/buildIconTag.js
+++ scripts/components/app_nav/tagBuilders/buildIconTag.js
@@ -1,9 +1,9 @@
 export function buildIconTag(wrapperClass, iconName) {
-    const ICONS_PATH = "assets/icons/app_nav";
+    const ICONS_PATH = "https://prajwalch.github.io/tutor.github.io/assets/icons/app_nav/";
     const ICON_EXTENSION = ".svg";
     const imgTagClass = `${wrapperClass}__icon`;
     const icon = `${iconName}${ICON_EXTENSION}`;
-    const fullPath = `${ICONS_PATH}/${icon}`;
+    const fullPath = `${ICONS_PATH}${icon}`;
     const imgTag = document.createElement("img");
     imgTag.setAttribute("class", imgTagClass);
     imgTag.setAttribute("src", fullPath);
